bdd260205e60c5d04bc2f743b71cbb26048a26eb
Cesar Sanchez <c_sanchez21@hotmail.com>
2020-10-21 17:00:14 -0700

    Added Solution

---
+++ DecompressRunLengthEncodedList/DecompressRunLengthEncodedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecompressRunLengthEncodedList
+{
+    //Problem: https://leetcode.com/problems/decompress-run-length-encoded-list/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in DecompressRLElist(new int[] { 1, 2, 3, 4 }))
+                Console.Write($" {i} ");
+        }
+
+        public static int[] DecompressRLElist(int[] nums)
+        {
+            List<int> results = new List<int>();
+            for(int i = 0; i < nums.Length-1; i += 2)
+            {
+                for (int j = 0; j < nums[i]; j++)
+                    results.Add(nums[i + 1]);
+            }
+            return results.ToArray();
+        }
+    }
+}
