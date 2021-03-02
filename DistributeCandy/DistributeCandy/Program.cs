a1118c1194da1aaee56b89258c4b1c6d314c6147
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-03-01 19:39:29 -0800

    Solution Added

---
+++ DistributeCandy/DistributeCandy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DistributeCandy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DistributeCandies(new int[] { 1, 1, 2, 2, 3, 3 }));
+            Console.WriteLine(DistributeCandies(new int[] { 1, 1, 2, 3 }));
+            Console.WriteLine(DistributeCandies(new int[] { 6, 6, 6, 6 }));
+        }
+        public static int DistributeCandies(int[] candyType)
+        {
+            int max = candyType.Length / 2;
+            HashSet<int> types = new HashSet<int>();
+            for (int i = 0; i < candyType.Length; i++)
+            {
+                types.Add(candyType[i]);
+                if (types.Count == max)
+                    break;
+            }
+            return Math.Min(types.Count, max);
+        }
+    }
+}
