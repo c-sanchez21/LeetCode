849a6c71df4377e57fa9a6f4b214331e2c8188de
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-03-07 15:22:01 -0800

    Added Solution

---
+++ AMZ/3Sum/3Sum/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3Sum
+{
+    class Program
+    {
+        //https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2966/
+        //https://leetcode.com/problems/3sum/
+        static void Main(string[] args)
+        {
+            int[][] inputs = new int[][]
+                {new int[] {-1, 0, 1, 2, -1, -4}, new int[]{}, new int[] {0 }, new int[]{0,0,0 },
+                 new int[] {-1, -1, -4, -3,-2, 0, 2, 4, 5 } };
+            IList<IList<int>> output;
+            foreach(int[] input in inputs)
+            {
+                output = ThreeSum(input);
+                Console.Write("[ ");
+                foreach (IList<int> arr in output)
+                {
+                    Console.Write("[{0},{1},{2}] ", arr[0], arr[1], arr[2]);
+                }
+                Console.WriteLine("]");
+            }
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            List<IList<int>> results = new List<IList<int>>();
+
+            if (nums == null || nums.Length < 3)
+                return results;
+
+            Array.Sort(nums);
+
+            int prev = -100001;
+            int l, r, curVal, sum, lo, hi;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                curVal = nums[i];
+                if (curVal == prev)
+                    continue;
+                if (curVal > 0)
+                    break;
+                l = i + 1;
+                r = nums.Length - 1;
+                while (l < r)
+                {
+                    lo = nums[l];
+                    hi = nums[r];
+                    sum = curVal + lo + hi;
+                    if (sum == 0)
+                    {
+                        results.Add(new List<int>(new int[] { curVal, lo, hi })) ;
+                        l++;
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1])
+                            l++;
+                    }
+                    else if (sum < 0)
+                        l++;
+                    else r--;
+                }
+                prev = curVal;
+            }
+            return results;
+        }
+    }
+}
---
+++ AMZ/IntegerToRoman/IntegerToRoman/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace IntegerToRoman
+{
+    class Program
+    {
+        //https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2964/
+        static void Main(string[] args)
+        {
+            int[] inputs = new int[] { 3, 4, 9, 58, 1994 };
+            foreach (int input in inputs)
+                Console.WriteLine(IntToRoman(input));
+        }
+
+        /*
+        Roman Numerals
+        Symbol       Value
+        I             1
+        IV            4
+        V             5
+        IX            9
+        X             10
+        XL            40
+        L             50
+        XC            90
+        C             100
+        CD            400
+        D             500
+        CM            900
+        M             1000
+        */
+        public static string IntToRoman(int num)
+        {
+            if (num >= 1000)
+                return "M" + IntToRoman(num - 1000);
+            else if (num >= 900)
+                return "CM" + IntToRoman(num - 900);
+            else if (num >= 500)
+                return "D" + IntToRoman(num - 500);
+            else if (num >= 400)
+                return "CD" + IntToRoman(num - 400);
+            else if (num >= 100)
+                return "C" + IntToRoman(num - 100);
+            else if (num >= 90)
+                return "XC" + IntToRoman(num - 90);
+            else if (num >= 50)
+                return "L" + IntToRoman(num - 50);
+            else if (num >= 40)
+                return "XL" + IntToRoman(num - 40);
+            else if (num >= 10)
+                return "X" + IntToRoman(num - 10);
+            else if (num >= 9)
+                return "IX" + IntToRoman(num - 9);
+            else if (num >= 5)
+                return "V" + IntToRoman(num - 5);
+            else if (num >= 4)
+                return "IV" + IntToRoman(num - 4);
+            else if (num >= 1)
+                return "I" + IntToRoman(num - 1);
+            return "";
+        }
+    }
+}
