a6510949816e5c3faa87d901c570a3bf6ccd8aaa
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-03-08 17:23:12 -0800

    Added Solution

---
+++ RemovePalindromicSubsequences/RemovePalindromicSubsequences/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RemovePalindromicSubsequences
+{
+    class Program
+    {
+        //https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3665/
+        //https://leetcode.com/problems/remove-palindromic-subsequences/
+        static void Main(string[] args)
+        {
+            string[] inputs = new string[] { "ababa", "abb", "baabb", "" };
+            foreach (string input in inputs)
+                Console.WriteLine("{0}={1}", input, RemovePalindromeSub(input));
+        }
+
+        public static int RemovePalindromeSub(string s)
+        {
+            if (String.IsNullOrEmpty(s)) return 0;
+            int l = 0;
+            int r = s.Length - 1;
+            while (l < r)
+                if (s[l++] != s[r--]) return 2;//If not palindrome
+            return 1;
+        }
+    }
+}
