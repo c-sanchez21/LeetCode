adadecef083222f27ff0afe8e52d0924c10ae0fd
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-05-07 12:55:53 -0700

    Solution Added

---
+++ Non-decreasing Array/Non-decreasing Array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Non_decreasing_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckPossibility(new int[] { 4, 2, 3 }));//True
+            Console.WriteLine(CheckPossibility(new int[] { 4, 2, 1 }));//False
+            Console.WriteLine(CheckPossibility(new int[] { 3, 4, 2, 3 }));//False
+            Console.WriteLine(CheckPossibility(new int[] { 5, 7, 1, 8 }));//True
+
+        }
+
+        //Given an array nums with n integers,
+        //your task is to check if it could become non-decreasing by modifying at most one element.
+        public static bool CheckPossibility(int[] nums)
+        {
+            for(int i = 0; i < nums.Length-1; i++)
+            {
+                //Find the first anomaly
+                if (nums[i + 1] - nums[i] < 0)
+                {
+                    //Anomaly found - Makes a copy of the array. (Yes, I know not optimal... :( )
+                    int[] a1 = new int[nums.Length];
+                    int[] a2 = new int[nums.Length];
+                    Array.Copy(nums, a1, nums.Length);
+                    Array.Copy(nums, a2, nums.Length);
+                    
+                    //Make the numbers the same - two choices. 
+                    a1[i] = nums[i + 1]; //First possiblity nums[i] = nums[i+1]
+                    a2[i + 1] = nums[i]; //Second possibilty nums[i+1] = nums[i]
+
+                    //Check if either possibility will work. 
+                    return CheckPossibility2(a1) || CheckPossibility2(a2);
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckPossibility2(int[] nums)
+        {
+            //Runs trough array and returns false when non-decreasing order is found. 
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i + 1] - nums[i] < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Subarray Sum Equals K/Subarray Sum Equals K/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subarray_Sum_Equals_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sub = SubarraySum(new int[] { 0, 3, -3,3}, 3);
+            Console.WriteLine(sub);
+        }
+
+        public static int SubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+            int sum = 0;
+            Dictionary<int, int> sumCounts = new Dictionary<int, int>();
+            sumCounts.Add(0, 1);
+            for(int i =0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (sumCounts.ContainsKey(sum - k))
+                    count += sumCounts[sum - k];
+                if (!sumCounts.ContainsKey(sum))
+                    sumCounts.Add(sum, 0);
+                sumCounts[sum]++;
+            }
+            return count;
+        }
+    }
+}
