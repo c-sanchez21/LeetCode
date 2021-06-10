308f70ae2fac35b0d80de3912a4bcc149fcf0350
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-06-09 23:31:10 -0700

    Added WPF Checkboxes

---
+++ Longest String Chain/Longest String Chain/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Longest_String_Chain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int ans = 1;//Default longest chain is 1
+        public int LongestStrChain(string[] words)
+        {
+            //Check for null and empty
+            if (words == null || words.Length == 0) return 0;
+
+            Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();//Holds paths for each word
+
+            //Sort words by length
+
+            //Check words for predecessor 
+            for (int i = 0; i < words.Length-1;i++)
+            {
+                for(int j = i+1; j < words.Length;j++)
+                {
+                    if (words[i].Length == words[j].Length)
+                        continue; //Skip words of same length
+                    else if (words[i].Length + 1 < words[j].Length)
+                        break; //words[j] is too big and cannot be successor. 
+                    else if(IsPredecessor(words[i], words[j]))
+                    {
+                        //If word has sucessor then map the paths each word can take
+                        if (!map.ContainsKey(words[i]))
+                            map.Add(words[i], new List<string>());
+                        map[words[i]].Add(words[j]);
+                    }
+                }
+            }
+
+          
+            foreach(string s in words)
+                DFS(s, map, 0); //Depth first search for the longest path;
+
+            return ans; 
+        }
+
+        static Dictionary<string, int> cache = new Dictionary<string, int>();//Memoization for longest string chain
+        public static int DFS(string s, Dictionary<string,List<string>> map, int depth)
+        {
+            if (cache.ContainsKey(s))
+                return depth += cache[s];
+
+            int max = depth + 1;
+            if (map.ContainsKey(s))
+            {
+                foreach (string s2 in map[s])
+                {
+                    int tmp = DFS(s2, map, depth + 1);
+                }
+            }
+        }
+        public static bool IsPredecessor(string s1, string s2)
+        {
+
+        }
+    }
+}
---
+++ Put Boxes Into the Warehouse I/Put Boxes Into the Warehouse I/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+//https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3735/
+namespace Put_Boxes_Into_the_Warehouse_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample case
+            Console.WriteLine(MaxBoxesInWarehouse(new int[] { 4, 3, 4, 1 }, new int[] { 5, 3, 3, 4, 1 }));//3
+        }
+
+        public static int MaxBoxesInWarehouse(int[] boxes, int[] warehouse)
+        {
+            //Check for invalid input. 
+            if (warehouse == null || warehouse.Length == 0) return 0;
+            if (boxes == null || boxes.Length == 0) return 0;
+
+            //We make note and keep track of the biggest box size that will fit 
+            //the back of the warehouse. 
+            int[] biggestFit = new int[warehouse.Length];
+            biggestFit[0] = warehouse[0];
+            for(int i =1; i < warehouse.Length; i++) //Take the smaller of the two - size of warehouse or size of box. 
+                biggestFit[i] = warehouse[i] < biggestFit[i - 1] ? warehouse[i] : biggestFit[i - 1];
+
+            //Sort the boxes from smallest to biggest. 
+            Array.Sort(boxes);
+
+
+            int idx = 0; //The index also serves as the number of boxes that fit. 
+
+            //Start from the back of the warehouse (where the smallest box will fit). 
+            for (int i = biggestFit.Length - 1; i >= 0; i--)
+            { 
+                //if the box fits push it in and increment idx/count. 
+                if(boxes[idx] <= biggestFit[i])
+                {
+                    idx++;
+                    if (idx >= boxes.Length) break;//If all boxes have been placed then stop. 
+                }
+            }
+            return idx; //return the count. 
+        }
+    }
+}
