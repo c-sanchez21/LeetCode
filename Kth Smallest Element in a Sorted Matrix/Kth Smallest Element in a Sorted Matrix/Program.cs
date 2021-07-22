33d5651b2485f6f761424857eb2c49c2b9bf3ba9
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-07-21 21:18:35 -0700

    Solution Added

---
+++ Kth Smallest Element in a Sorted Matrix/Kth Smallest Element in a Sorted Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kth_Smallest_Element_in_a_Sorted_Matrix
+{
+    class Program
+    {
+        //https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/608/week-1-july-1st-july-7th/3805/
+        static void Main(string[] args)
+        {
+            int[][] matrix = new int[][] { new int[] { 1, 5, 9 }, new int[] { 10, 11, 13 }, new int[] { 12, 13, 15 } };
+            Console.WriteLine(KthSmallest(matrix, 8)); //Should be 13. 
+        }
+
+        public static int KthSmallest(int[][] matrix, int k)
+        {
+            //Flatten matrix into a list
+            List<int> list = new List<int>();
+            for(int i = 0; i < matrix.Length; i++)
+                for(int j = 0; j < matrix[0].Length; j++)
+                {
+                    list.Add(matrix[i][j]);
+                }
+
+            //Sort the list
+            list.Sort();
+
+            //Return kth element
+            return list[k - 1]; 
+        }
+    }
+}
---
+++ Push Dominoes/Push Dominoes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Push_Dominoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PushDominoes(".L.R...LR..L..")); //"LL.RR.LLRRLL.."
+        }
+
+        public static string PushDominoes(string dominoes)
+        {
+            int n = dominoes.Length;
+            int[] forces = new int[n];
+
+            int f = 0; //Force
+            //Check forces left to right
+            for(int i = 0; i < n; i++)
+            {
+                if (dominoes[i] == 'R')
+                    f = n;
+                else if (dominoes[i] == 'L')
+                    f = 0;
+                else f = Math.Max(f - 1, 0);
+                forces[i] += f; 
+            }
+
+            //Check forces right to left 
+            f = 0;
+            for(int i = n-1; i >= 0; i--)
+            {
+                if (dominoes[i] == 'L')
+                    f = n;
+                else if (dominoes[i] == 'R')
+                    f = 0;
+                else f = Math.Max(f - 1, 0);
+                forces[i] -= f;
+            }
+
+            //Calculate answer
+            StringBuilder s = new StringBuilder();
+            foreach (int x in forces)
+            {
+                if (x > 0)
+                    s.Append('R');
+                else if (x < 0)
+                    s.Append('L');
+                else //x == 0
+                    s.Append('.');
+            }
+            return s.ToString();
+        }
+    }
+}
