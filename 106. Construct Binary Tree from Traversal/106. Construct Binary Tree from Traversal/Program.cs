acf21ee4283dfa9c0b795933c1f8406974718a54
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-11-21 16:20:02 -0800

    Solution Added

---
+++ 106. Construct Binary Tree from Traversal/106. Construct Binary Tree from Traversal/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _106._Construct_Binary_Tree_from_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inorder = [9, 3, 15, 20, 7], postorder = [9, 15, 7, 20, 3]
+            TreeNode n = BuildTree(new int[] { 9, 3, 15, 20, 7 }, new int[] { 9, 15, 7, 20, 3 });
+            PrintTree(n);//Root, Left, Right...,
+        }
+
+        //Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        static int idxPost;
+        static int[] InOrder;
+        static int[] PostOrder;
+        static Dictionary<int, int> IdxMap = new Dictionary<int, int>();
+        public static TreeNode BuildTree(int[] inorder, int[] postorder)
+        {
+            InOrder = inorder;
+            PostOrder = postorder;
+            idxPost = postorder.Length - 1;//Start with Last Element
+
+            //Build map of the InOrder traversal
+            for (int i = 0; i < inorder.Length; i++)
+                if (!IdxMap.ContainsKey(inorder[i]))
+                    IdxMap.Add(inorder[i], i);
+                else IdxMap[inorder[i]] = i; 
+
+            return BuildTreeSub(0, inorder.Length - 1);
+
+        }
+
+        public static TreeNode BuildTreeSub(int l, int r)
+        {
+            //If Inorder L > R
+            if (l > r) return null;
+
+            int rootVal = PostOrder[idxPost];
+            TreeNode root = new TreeNode(rootVal);
+
+            //Root value determines left & right subTrees
+            int idx = IdxMap[rootVal];
+            idxPost--;
+
+            //Build Right SubTree
+            root.right = BuildTreeSub(idx + 1, r);
+            //Build Left SubTree
+            root.left = BuildTreeSub(l, idx - 1);
+            return root;
+        }
+
+
+        public static void PrintTree(TreeNode n)
+        {
+            Console.Write("[");
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(n);
+
+            while (q.Count > 0)
+            {
+                n = q.Dequeue();
+                Console.Write(n.val);
+                if (n.left != null) q.Enqueue(n.left);
+                if (n.right != null) q.Enqueue(n.right);
+                if (q.Count > 0)
+                    Console.Write(",");
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ 540. Single Element in a Sorted Array/540. Single Element in a Sorted Array/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _540._Single_Element_in_a_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingleNonDuplicate(new int[] { 1, 2, 2 }));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 1, 1, 2, 3, 3 }));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 2, 2, 3 }));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 1, 2, 2, 3, 3}));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 1, 1, 2, 2, 3 }));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 }));
+            Console.WriteLine(SingleNonDuplicate(new int[] { 3, 3, 7, 7, 10, 11, 11 }));
+        }
+
+        //O(log n) Solution - Divide & Conquer - Binary Search
+        public static int SingleNonDuplicate2(int[] nums)
+        {
+            return SingleNonDuplicate(nums, 0, nums.Length - 1);
+        }
+
+        public static int SingleNonDuplicate(int[] nums, int l, int r)
+        {
+            //TODO: Implement
+            return 0;
+        }
+
+
+
+        //O(n) Solution
+        public static int SingleNonDuplicate(int[] nums)
+        {
+            int max = nums.Length;
+            for (int i = 1; i < max-1; i += 2)
+            {
+                if(nums[i] != nums[i-1]) //Not equal left
+                {
+                    if (i == nums.Length - 1 || nums[i] != nums[i + 1]) //And not equal right
+                        return nums[i]; //Return current
+                    else return nums[i - 1]; //Return left
+                }
+            }
+            return nums[max - 1];//Return last number
+        }
+    }
+}
