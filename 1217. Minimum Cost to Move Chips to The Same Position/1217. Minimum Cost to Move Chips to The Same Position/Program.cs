5743b4e5afa68c59d9615417d6bedecde6f0a9cf
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-12-06 09:31:13 -0800

    Solution Add

---
+++ 1217. Minimum Cost to Move Chips to The Same Position/1217. Minimum Cost to Move Chips to The Same Position/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1217._Minimum_Cost_to_Move_Chips_to_The_Same_Position
+{
+    //https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinCostToMoveChips(new int[] { 1, 2, 3 }));
+            Console.WriteLine(MinCostToMoveChips(new int[] { 2, 2, 2, 3,3 }));
+            Console.WriteLine(MinCostToMoveChips(new int[] { 1, 1000000000 }));
+        }
+
+        public static int MinCostToMoveChips(int[] position)
+        {
+            //Since the cost of moving two spaces is free (Odd + 2 = Odd, Even + 2 = Even)
+            //The question becomes How many odds vs even - then return the Min between them. 
+            int odd = 0;
+            int even = 0;
+            //Iterate thru entire array and count the odds and evens
+            for (int i = 0; i < position.Length; i++)
+                if (position[i] % 2 == 0)
+                    even++;
+                else odd++;
+            //Return the Min
+            return Math.Min(odd, even);
+        }
+    }
+}
