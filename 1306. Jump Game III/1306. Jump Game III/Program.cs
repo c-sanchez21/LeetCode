c84d637d41e9e875c8d534d5123b0a723dd6c64d
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-12-09 14:24:23 -0800

    Solution Added

---
+++ 1306. Jump Game III/1306. Jump Game III/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1306._Jump_Game_III
+{
+    class Program
+    {
+        //https://leetcode.com/problems/jump-game-iii/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanReach(new int[] { 4, 2, 3, 0, 3, 1, 2 },5));
+        }
+
+        //Depth First Search
+        public static bool CanReach(int[] arr, int start)
+        {
+            //Check for out of bounds or already visited
+            if (start >= 0 && start < arr.Length && arr[start] >= 0)
+            {
+                //Target found - return true
+                if (arr[start] == 0) return true;
+
+                //Mark as visisted
+                arr[start] = arr[start] * -1;
+
+                //Depth First Search 
+                return CanReach(arr, start + arr[start]) || CanReach(arr, start - arr[start]);
+            }
+            return false;
+        }
+    }
+}
