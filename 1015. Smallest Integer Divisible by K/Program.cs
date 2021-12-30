61c47b4fde169f19de44e6ed7e8db51a5401a492
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-12-30 09:26:18 -0800

    Solution Added

---
+++ 1015. Smallest Integer Divisible by K/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1015._Smallest_Integer_Divisible_by_K
+{
+    class Program
+    {
+        //https://leetcode.com/problems/smallest-integer-divisible-by-k/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallestRepunitDivByK(1));
+            Console.WriteLine(SmallestRepunitDivByK(2));
+            Console.WriteLine(SmallestRepunitDivByK(3));
+        }
+        public static int SmallestRepunitDivByK(int k)
+        {
+            int length = 1;//Length of N
+            int mod = 1;//Remainder
+
+            //Remainders already seen to prevent cycles. 
+            HashSet<int> remainders = new HashSet<int>();
+
+            int n;
+            while(mod%k != 0) 
+            {
+                n = mod * 10 + 1;
+                mod = n % k;
+                length++;
+                if (remainders.Contains(mod))
+                    return -1; //Endless Cycle detected
+                else
+                    remainders.Add(mod);
+            }
+            return length;
+        }
+    }
+}
