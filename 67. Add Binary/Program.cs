27704aa413b19c49e15e5cdc6ee71e6360d88311
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-01-10 15:04:00 -0800

    Solution Added

---
+++ 67. Add Binary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace _67._Add_Binary
+{
+    class Program
+    {
+        //https://leetcode.com/problems/add-binary/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary("11", "1"));
+            Console.WriteLine(AddBinary("1111", "11111"));
+            Console.WriteLine(AddBinary("1010", "1011"));
+        }
+
+        public static string AddBinary(string a, string b)
+        {
+            //Find the longer and shorter of the strings
+            string shorter, longer;
+            if(a.Length >= b.Length)
+            {
+                longer = a;
+                shorter = b;
+            }
+            else
+            {
+                longer = b;
+                shorter = a;
+            }
+            
+            //Compute the results
+            StringBuilder res = new StringBuilder(); //Result
+            int carry = 0;
+            int i = longer.Length-1;
+            int j = shorter.Length-1;
+            for(; i >= 0; i--)
+            {
+                if (longer[i] == '1') carry++;
+                if (j >= 0 && shorter[j--] == '1') carry++;
+                if (carry % 2 == 1) res.Append('1');
+                else res.Append('0');
+                carry /= 2;
+            }
+            if (carry == 1) res.Append('1');
+
+            //Reverse string and return answer
+            char[] charArray = res.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            string ans = new string(charArray);
+            return ans;
+        }
+    }
+}
---
+++ 997. Find the Town Judge/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _997._Find_the_Town_Judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        public static int FindJudge(int n, int[][] trust)
+        {
+            //Everyone trusts the judge else no judge
+            if (trust.Length < n - 1) return -1;
+
+
+            int[] indegrees = new int[n + 1];//Edges coming in
+            int[] outdegrees = new int[n + 1];//Edges going out
+
+            foreach(int[] relation in trust)
+            {
+                outdegrees[relation[0]]++;
+                indegrees[relation[1]]++;
+            }
+
+            //Find the Judge 
+            for (int i = 1; i <= n; i++)
+                //Everyone trusts the Judge and the judge trust no one.
+                if (indegrees[i] == n - 1 && outdegrees[i] == 0)
+                    return i;
+
+            return -1;
+        }
+    }
+}
