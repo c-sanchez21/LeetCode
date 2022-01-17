aafe89b7f1fd205e027b6f46f9d9cfe23f7bd2be
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-01-17 12:50:40 -0800

    Solution Added

---
+++ 290. Word Pattern/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _290._Word_Pattern
+{
+    class Program
+    {
+        //https://leetcode.com/problems/word-pattern/solution/
+        static void Main(string[] args)
+        {
+            //Example Cases
+            Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
+            Console.WriteLine(WordPattern("abba", "dog cat cat fish"));
+            Console.WriteLine(WordPattern("aaaa", "dog cat cat dog"));
+            Console.WriteLine(WordPattern("abba", "dog dog dog dog"));
+        }
+
+        public static bool WordPattern(string pattern, string str)
+        {
+            //Check for null or empty
+            if (String.IsNullOrEmpty(pattern) && string.IsNullOrEmpty(str))
+                return true; //Both Empty return true;
+            else if (String.IsNullOrEmpty(pattern)) return false;
+            else if (String.IsNullOrEmpty(str)) return false;
+
+            //Check counts for consistency
+            string[] words = str.Split(' ');
+            if (pattern.Length != words.Length) return false;
+
+            Dictionary<char, string> map = new Dictionary<char, string>();//Char to Word Map
+            HashSet<string> usedWords = new HashSet<string>();//Track words that have been used
+            char c; string word;
+
+            //Check if the Pattern matches Word Pattern
+            for (int i= 0; i < pattern.Length; i++)
+            {
+                c = pattern[i];
+                word = words[i];
+                if (!map.ContainsKey(c))
+                {
+                    //Word already used;
+                    if (usedWords.Contains(word))
+                        return false;
+
+                    //Map new word
+                    map.Add(c, word);
+                    usedWords.Add(word);
+                }
+                else if (map[c].CompareTo(word) != 0) return false;
+            }
+
+            //Return true
+            return true;
+
+        }
+    }
+}
---
+++ 849. Maximize Distance to Closest Person/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _849._Maximize_Distance_to_Closest_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxDistToClosest(new int[] { 1, 0, 0, 0, 1, 0, 1 }));
+            Console.WriteLine(MaxDistToClosest(new int[] { 1, 0, 0, 0 }));
+            Console.WriteLine(MaxDistToClosest(new int[] { 0, 1 }));
+        }
+
+        public static int MaxDistToClosest(int[] seats)
+        {
+            //Scores to hold the distance
+            int[] scores = new int[seats.Length];
+            
+            //Initialize scores to max value
+            for (int i = 0; i < scores.Length; i++)
+                scores[i] = int.MaxValue;
+
+            //Calculate scores Left to Right
+            int val = int.MaxValue;//Score Value
+            for(int i = 0;i < seats.Length; i++)
+            {
+                if (seats[i] == 1)//If person seated
+                    val = 0; //Reset Score to Zero 
+                else if (val < int.MaxValue)
+                    val++;//Else increment score
+                scores[i] = Math.Min(scores[i], val);
+            }
+
+            //Repeate Process from Right to Left
+            val = int.MaxValue;
+            for(int i = seats.Length-1; i >= 0; i--)
+            {
+                if (seats[i] == 1)
+                    val = 0;
+                else if (val < int.MaxValue)
+                    val++;
+                scores[i] = Math.Min(scores[i], val);
+            }
+
+            val = int.MinValue;
+            for (int i = 0; i < scores.Length; i++)
+                val = Math.Max(scores[i], val);
+
+            return val;
+        }
+    }
+}
