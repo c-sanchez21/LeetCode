bb702f806c9cfda3d4b3d5febe3350b1d14e7c04
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-01-19 12:34:16 -0800

    Solution Added

---
+++ 142. Linked List Cycle II/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _142._Linked_List_Cycle_II
+{
+    class Program
+    {
+        //https://leetcode.com/problems/linked-list-cycle-ii/
+        static void Main(string[] args)
+        {
+            //Example 1:
+            ListNode n1 = new ListNode(3);
+            n1.next = new ListNode(2);
+            n1.next.next = new ListNode(0);
+            n1.next.next.next = new ListNode(-4);
+            n1.next.next.next.next = n1.next;
+            ListNode res1 = DetectCycle(n1);
+            Console.WriteLine("{0}", res1);
+
+            //Example 2:
+            ListNode n2 = new ListNode(1);
+            n2.next = new ListNode(2);
+            n2.next.next = new ListNode(3);
+            n2.next.next.next = n2;
+            ListNode res2 = DetectCycle(n2);
+            Console.WriteLine("{0}", res2);
+        }
+        
+
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+
+            public override string ToString()
+            {
+                return String.Format("[{0}]", val);
+            }
+        }
+
+
+        public static ListNode DetectCycle(ListNode head)
+        {
+            //Floyd Tortoise and Hare
+            ListNode slow, fast;
+            slow = fast = head;
+
+            //Find Intersection 
+            while(fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    break;//Intersection Found  
+            }
+
+            //No Intersection - No Cycle - Return Null
+            if (fast == null || fast.next == null)
+                return null;
+
+            //Increment both by 1 - They will meet at the entrance
+            slow = head;
+            while(slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+        }
+    }
+}
---
+++ 605. Can Place Flowers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _605._Can_Place_Flowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanPlaceFlowers(new int[] { 0, 0, 0, 0, 0 }, 3));
+        }
+
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int i = 0, count = 0;
+            while (i < flowerbed.Length)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+                i++;
+            }
+            return count >= n;
+        }
+    }
+}
