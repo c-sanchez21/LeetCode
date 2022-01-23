ef8ef20d005b7314d182f94359ab4b0512e4617e
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-01-23 15:41:13 -0800

    Solution Add

---
+++ 1291. Sequential Digits/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1291._Sequential_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exmaple 1:");
+            foreach (int num in SequentialDigits(100, 300))
+                Console.WriteLine(num.ToString());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Example 2:");
+            foreach (int num in SequentialDigits(1000, 13000))
+                Console.WriteLine(num);
+           
+        }
+
+        public static IList<int> SequentialDigits(int low, int high)
+        {
+            IList<int> results = new List<int>();
+            string numbers = "123456789";
+
+            //Length of Numbers / Number of digits 
+            int min = low.ToString().Length;
+            int max = high.ToString().Length;
+
+            //Grab all the Sequential digit strings from length min to max
+            int num;
+            for(int i = min; i < max+1;i++)//From length of low to length of high
+            {
+                for(int start = 0; start < 10-i; ++start)
+                {
+                    num = int.Parse(numbers.Substring(start, i));
+                    //Add numbers that are in [low,high] 
+                    if (num >= low && num <= high) results.Add(num);
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ 1510. Stone Game IV/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1510._Stone_Game_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine("N={0} => {1}", i, WinnerSquareGame(i));
+                
+        }
+
+        public static bool WinnerSquareGame(int n)
+        {
+            bool[] dp = new bool[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                for (int k = 1; k * k <= i; k++)
+                {
+                    if (dp[i - k * k] == false)
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[n];
+        }
+    }
+}
