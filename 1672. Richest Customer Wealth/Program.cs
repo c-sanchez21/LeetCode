47640df14debffe11c38e9f4e659d3a59ae5c1bf
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-01-31 15:15:22 -0800

    Solution Added

---
+++ 1672. Richest Customer Wealth/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1672._Richest_Customer_Wealth
+{
+    class Program
+    {
+        //https://leetcode.com/problems/richest-customer-wealth/
+        static void Main(string[] args)
+        {
+            //Example 1
+            int[][] accounts1 = new int[][]
+            {
+                new int[] {1,2,3 },
+                new int[] {3,2,1 }
+            };
+            Console.WriteLine(MaximumWealth(accounts1));
+
+            //Example 2
+            int[][] accounts2 = new int[][]
+            {
+                new int[] {1,5 },
+                new int[] {7,3 },
+                new int[] {3,5 }
+            };
+            Console.WriteLine(MaximumWealth(accounts2));
+        }
+
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int max = 0;
+            int sum;
+            //Iterate thru all the rows and colums
+            for(int i =0; i < accounts.Length; i++)
+            {
+                //Find sum
+                sum = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                    sum += accounts[i][j];
+                max = Math.Max(sum, max); //Take the max value
+            }
+            return max; //Return max
+        }
+    }
+}
---
+++ 520. Detect Capital/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _520._Detect_Capital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DetectCapitalUse("USA"));
+            Console.WriteLine(DetectCapitalUse("FlaG"));
+            Console.WriteLine(DetectCapitalUse("Programming"));
+            Console.WriteLine(DetectCapitalUse("assembly"));
+        }
+
+        public static bool DetectCapitalUse(string word)
+        {
+            //Check for invalid input
+            if (string.IsNullOrEmpty(word)) return false; 
+
+            //All lowercase
+            string lowerCase = word.ToLower();
+            if (word.CompareTo(lowerCase) == 0) return true;
+
+            //All uppercase
+            string upperCase = word.ToUpper();
+            if (word.CompareTo(upperCase) == 0) return true;
+
+            //First letter capitalized and the rest lowercase 
+            int length = word.Length;
+            if (word[0] == upperCase[0] && word.Substring(1, length - 1).CompareTo(lowerCase.Substring(1, length - 1)) == 0)
+                return true;
+
+            //Invalid capital use
+            return false;
+        }
+    }
+}
