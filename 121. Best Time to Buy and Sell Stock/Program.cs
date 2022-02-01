34ea5056c0b963bbf7d3044876b00588e8f3e4d1
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-01 08:04:34 -0800

    Solution Added

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _121._Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Cases
+            Console.WriteLine(MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+            Console.WriteLine(MaxProfit(new int[] { 7, 6, 4, 3, 1 }));
+            Console.WriteLine(MaxProfit(new int[] { 2,6,1,2,2,3 }));
+
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            //Check for invalid input
+            if (prices == null || prices.Length == 0) return 0;
+
+            int min = prices[0]; //Current min price
+            int maxProfit = 0; //Current max profit
+
+            //Find the max profit
+            for(int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > min) //Update the maxProfit
+                    maxProfit = Math.Max(prices[i] - min, maxProfit);
+                else min = prices[i];//Or else update the new min
+            }
+
+            return maxProfit;
+        }
+    }
+}
