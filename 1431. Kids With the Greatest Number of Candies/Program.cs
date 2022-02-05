27ddca04c2dce95c6901e1035afe898a1d23ba07
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-05 12:41:05 -0800

    Solution Added'

---
+++ 1431. Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1431._Kids_With_the_Greatest_Number_of_Candies
+{
+    class Program
+    {
+        //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+        static void Main(string[] args)
+        {
+            //Example 1: 
+            IList<bool> results = KidsWithCandies(new int[] { 12, 1, 12 }, 10);
+            Console.WriteLine("{0}", string.Join(" ", new List<bool>(results).ConvertAll(i => i.ToString()).ToArray()));
+        }
+
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            //Find the max value - the kid with greatest amount of candies
+            int max = 0;
+            for (int i = 0; i < candies.Length; i++)
+                max = Math.Max(max, candies[i]);
+
+            //Find if giving the ith kid the extra candies will make them the one with most candies
+            List<bool> results = new List<bool>();
+            for (int i = 0; i < candies.Length; i++)
+                results.Add((candies[i] + extraCandies >= max) ? true : false);
+
+            return results;
+        }
+    }
+}
