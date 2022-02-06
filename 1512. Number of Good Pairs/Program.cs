4ac870e7954254a375f88d418f8d2a16c0fd4b50
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-06 12:44:09 -0800

    Solution Added

---
+++ 1512. Number of Good Pairs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1512._Number_of_Good_Pairs
+{
+    class Program
+    {
+        //https://leetcode.com/problems/number-of-good-pairs/
+        static void Main(string[] args)
+        {
+            //Example 1: 
+            int pairs = NumIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 });
+            Console.WriteLine(pairs);
+        }
+
+        //Returns numbers of pairs where nums[i] == nums[j] & i < j
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int pairs = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                    if (nums[i] == nums[j])
+                        pairs++;
+            return pairs;
+        }
+    }
+}
