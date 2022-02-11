ac665da45b8ca4c9a736ff0f5a0c0b3cc2a1c102
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-10 17:06:30 -0800

    Solution Added

---
+++ 560. Subarray Sum Equals K/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _560._Subarray_Sum_Equals_K
+{
+    class Program
+    {
+        //https://leetcode.com/problems/subarray-sum-equals-k/
+        //Given an array of integers nums and an integer k,
+        //return the total number of continuous subarrays whose sum equals to k.
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine( SubarraySum(new int[] { 1, 1, 1 }, 2));
+            Console.WriteLine(SubarraySum(new int[] { 1, 2, 3 },3));
+        }
+
+        public static int SubarraySum(int[] nums, int k)
+        {
+            //Map for keeping track of running sums
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            map.Add(0, 1); //Add 0 as default, i.e sum-k = 0 or Sum = k
+            int sum = 0;
+            int count = 0;
+
+            //Iterate over all the numbers 
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i]; //Keep track of the sum
+                if (map.ContainsKey(sum - k))
+                    count += map[sum-k]; //Count the numbers of times we can add up to k
+
+                //Add sum to the hashmap
+                if (!map.ContainsKey(sum))
+                    map.Add(sum, 0);
+                map[sum]++;
+            }
+            return count; //Return count
+        }
+    }
+}
---
+++ 567. Permutation in String/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _567._Permutation_in_String
+{
+    class Program
+    {
+        //https://leetcode.com/problems/permutation-in-string/
+        //Find if s2 contains a permutation of s1
+
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine(CheckInclusion("ab", "eidbaooo"));
+            Console.WriteLine(CheckInclusion("ab", "eidboaoo"));
+            Console.WriteLine(CheckInclusion("", "aaaaaaaaaa"));
+            Console.WriteLine(CheckInclusion("a", "aaaaaa"));
+            Console.WriteLine(CheckInclusion("abb", "abab"));
+            Console.WriteLine(CheckInclusion("abb", "aba"));
+            Console.WriteLine(CheckInclusion("abc", "cab"));
+
+        }
+
+        //Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+        public static bool CheckInclusion(string s1, string s2)
+        {
+            //Check invalid input
+            if (String.IsNullOrEmpty(s1) || String.IsNullOrEmpty(s2)) return false;
+            if (s1.Length > s2.Length) return false;
+
+            //Case of only 1 letter 
+            if (s1.Length == 1) return s2.Contains(s1);
+
+            //Hashmap to hold letters from s1
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach(char c in s1)
+            {
+                if (!map.ContainsKey(c))
+                    map.Add(c, 0);
+                map[c]++;
+            }
+
+            int count = 0; //Letters Found
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            char ch;
+            int s1Length = s1.Length;
+            for (int i = 0; i < s2.Length; i++)
+            {
+                ch = s2[i];
+                if (map.ContainsKey(ch))
+                {
+                    if (!counts.ContainsKey(ch))
+                        counts.Add(ch, 0);
+                    counts[ch]++;
+                    count++;
+                    if(count == s1.Length)
+                    {
+                        if (map.Count == counts.Count && !map.Except(counts).Any())
+                            return true;
+                        else //Remove last letter on the left
+                        {
+                            char chLeft = s2[i - (s1Length - 1)];
+                            counts[chLeft]--;
+                            count--;
+                        }
+                    }
+                }
+                else
+                {
+                    count = 0;
+                    counts.Clear();
+                }
+            }
+            return false;
+        }
+    }
+}
