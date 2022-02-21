f210fa56be6acf979806eeefc77c277079d1d6b9
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-21 14:02:11 -0800

    Solution Added

---
+++ 1104. Path In Zigzag Labelled Binary Tree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1104._Path_In_Zigzag_Labelled_Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int x in PathInZigZagTree(14))
+                Console.Write(" {0} ", x);
+        }
+        public static IList<int> PathInZigZagTree(int label)
+        {
+            IList<int> results = new List<int>();
+            int rowsNeed = (int)Math.Log2(label) + 1;
+            Console.WriteLine("Rows Needed = {0}", rowsNeed);
+            int node = 1;
+            int nodesNeeded = 1;
+            Stack<int> nodeStack = new Stack<int>();
+            bool rowEven = false;
+            for(int row = 1; row <= rowsNeed; row++)
+            {
+                rowEven = (row % 2) == 0;
+                for(int i = 0; i < nodesNeeded; i++)
+                {
+                    if (rowEven)
+                        nodeStack.Push(node);
+                    else results.Add(node);
+                    if (node == label)
+                        break;
+                    node++;
+                }
+                while (nodeStack.Count > 0)
+                    results.Add(nodeStack.Pop());
+                nodesNeeded *= 2;
+            }
+            return results;
+        }
+    }
+}
---
+++ 169. Majority Element/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _169._Majority_Element
+{
+    class Program
+    {
+        //https://leetcode.com/problems/majority-element/
+        static void Main(string[] args)
+        {
+            //Example 
+            Console.WriteLine(MajorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            Array.Sort(nums);
+            //Since majority element appears more than n/2 times
+            return nums[nums.Length / 2];
+        }
+    }
+}
