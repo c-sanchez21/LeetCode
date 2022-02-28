fa94e1df9074870adb1b2677a13a3e3337a20ffa
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-02-28 15:52:38 -0800

    Solution Added

---
+++ 228. Summary Ranges/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _228._Summary_Ranges
+{
+    //https://leetcode.com/problems/summary-ranges/submissions/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example 1
+            Console.WriteLine(String.Join(' ', SummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 })));
+
+            //Example 2
+            Console.WriteLine(String.Join(' ', SummaryRanges(new int[] { 0, 2, 3, 4, 6, 8,9 })));
+        }
+
+        public static List<String> SummaryRanges(int[] nums)
+        {
+            List<string> results = new List<string>();
+
+            //Check invalid results
+            if (nums == null || nums.Length == 0) return results;
+
+            int j;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                j = i;
+                //While numbers are in ascending order and differ by 1
+                while (j + 1 < nums.Length && nums[j] + 1 == nums[j + 1])
+                    j++;
+
+                if (i == j)
+                    results.Add(nums[i].ToString()); //Add single number
+                else results.Add(nums[i] + "->" + nums[j]); //Add Range
+                i = j;
+            }
+            return results;
+        }
+    }
+}
