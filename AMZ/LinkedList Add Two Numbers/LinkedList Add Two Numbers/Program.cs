a05d846c45344a233ba50ca4ad9d463c12375d2f
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-03-10 09:44:01 -0800

    Solution Refactored

--- AMZ/LinkedList Add Two Numbers/LinkedList Add Two Numbers/Program.cs
+++ AMZ/LinkedList Add Two Numbers/LinkedList Add Two Numbers/Program.cs
@@ -7,11 +7,11 @@ namespace LinkedList_Add_Two_Numbers
         static void Main(string[] args)
         {
             ListNode l1 = new ListNode(2,
-                new ListNode(4,
-                new ListNode(3)));
+                new ListNode(4,new ListNode(3)));
+
             ListNode l2 = new ListNode(5,
-                new ListNode(6,
-                new ListNode(4)));
+                new ListNode(6,new ListNode(4)));
+
             PrintLinkedList(AddTwoNumbers(l1, l2));
         }
 
