b07010617c0032e9ebc5de4e4a601f790b1f9b13
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-03-16 07:03:18 -0700

    Solution Added

---
+++ 1249. Minimum Remove to Make Valid Parentheses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1249._Minimum_Remove_to_Make_Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine(MinRemoveToMakeValid("lee(t(c)o)de)"));
+            Console.WriteLine(MinRemoveToMakeValid("a)b(c)d"));
+            Console.WriteLine(MinRemoveToMakeValid("))(("));
+        }
+
+        public static string MinRemoveToMakeValid(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            Stack<int> stack = new Stack<int>();
+            char c;
+            int idx = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                c = s[i];
+                if (c == '(')
+                {
+                    stack.Push(idx++);
+                    sb.Append(c);
+                    continue;
+                }
+                else if (c == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                        sb.Append(c);
+                        idx++;
+                        continue;
+                    }
+                    else continue; //Skip
+                }
+                sb.Append(c);
+                idx++;
+            }
+            while (stack.Count > 0)
+                sb.Remove(stack.Pop(), 1);
+            return sb.ToString();
+        }
+    }
+}
---
+++ 946. Validate Stack Sequences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _946._Validate_Stack_Sequences
+{
+    class Program
+    {
+        //https://leetcode.com/problems/validate-stack-sequences/
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine(ValidateStackSequences(new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 4, 5, 3, 2, 1 }));
+
+            Console.WriteLine(ValidateStackSequences(new int[] { 1, 2, 3, 4, 5 },
+          new int[] { 4, 3,5, 1, 2 }));
+
+        }
+
+        public static bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            int idx = 0;
+            Stack<int> stack = new Stack<int>();
+            for(int i =0; i < pushed.Length; i++)
+            {
+                stack.Push(pushed[i]);
+                while(stack.Count > 0 && popped[idx] == stack.Peek())
+                {
+                    stack.Pop();
+                    idx++;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
