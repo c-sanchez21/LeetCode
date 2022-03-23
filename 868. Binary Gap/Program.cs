e505b4346c65f8d2fae975f842c6dbaf901d11e7
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-03-22 17:10:31 -0700

    Solution Added

---
+++ 868. Binary Gap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _868._Binary_Gap
+{
+    class Program
+    {
+        //https://leetcode.com/problems/binary-gap/
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine(BinaryGap(22));
+            Console.WriteLine(BinaryGap(8));
+            Console.WriteLine(BinaryGap(5));
+        }
+
+        public static int BinaryGap(int n)
+        {
+            if (n == 0) return 0;
+
+            int gap = 0;
+            int cur = -1;
+            while(n > 0)
+            {
+                if ((n & 1) == 1)
+                {
+                    gap = Math.Max(gap, cur);
+                    cur = 1;
+                }
+                else if(cur != -1) cur++;
+                n  = n >> 1;
+            }
+            return gap;
+
+        }
+    }
+}
