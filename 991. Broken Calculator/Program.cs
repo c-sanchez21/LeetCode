b5bd6e5181dee3dde03cd97ff939739af846b830
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-03-22 18:13:58 -0700

    Solution Added

---
+++ 991. Broken Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _991._Broken_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(BrokenCalc(2, 3));
+           Console.WriteLine(BrokenCalc(5, 8));
+           Console.WriteLine(BrokenCalc(3, 10));
+        }
+
+        public static int BrokenCalc(int startValue, int target)
+        {
+            int ans = 0;
+            //Work backwards
+            while(target > startValue)
+            {
+                ans++;
+                //Make target even
+                if (target % 2 == 1)
+                    target++;
+                else target /= 2; //Greedily divide by 2
+            }
+            return ans + (startValue - target);
+        }
+
+
+    }
+}
