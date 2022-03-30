e1cc719ef1e082ee58491d1dc3db0379bbd35a4b
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-03-29 17:57:35 -0700

    Solution Added

---
+++ 287. Find the Duplicate Number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _287._Find_the_Duplicate_Number
+{
+    class Program
+    {
+        //https://leetcode.com/problems/find-the-duplicate-number/
+        static void Main(string[] args)
+        {
+            //Examples
+            Console.WriteLine(FindDuplicate(new int[] { 1, 3, 4, 2, 2 }));
+            Console.WriteLine(FindDuplicate(new int[] { 3, 1, 3, 4, 2 }));
+        }
+
+        public static int FindDuplicate(int[] nums)
+        {
+            //Find the largest number
+            int maxNum = 0;
+            for (int i = 0; i < nums.Length; i++)
+                maxNum = Math.Max(maxNum, nums[i]);
+
+            //Find most significant bit of the largest number
+            int maxBit = 0;
+            while(maxNum > 0)
+            {
+                maxBit++;
+                maxNum = maxNum >> 1; // = maxNum/2;
+            }
+
+            int dup = 0;
+            for(int bit = 0; bit < maxBit; bit++) //Iterate thru all bits
+            {
+                int mask = (1 << bit); //Set bit mask
+                int baseCount = 0, numsCount = 0;
+                for(int i =0; i < nums.Length; i++)
+                {
+                    if ((i & mask) > 0)
+                        baseCount++;
+                    if ((nums[i] & mask) > 0)
+                        numsCount++;
+                }
+                if (numsCount > baseCount)
+                    dup |= mask;
+            }
+            return dup;
+        }
+    }
+}
---
+++ 81. Search in Rotated Sorted Array II/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _81._Search_in_Rotated_Sorted_Array_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Search(new int[] { 2, 5, 6, 0, 0, 1, 2 }, 0));
+        }
+
+
+        //O(n) - Linear Search
+        public static bool Search(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] == target) return true;
+            return false;
+        }
+    }
+}
