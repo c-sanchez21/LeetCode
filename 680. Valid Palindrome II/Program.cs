5eeb314e6c8ad2b0193d86dd36611a3d221c5d1e
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-04-01 23:55:07 -0700

    Solution Added

---
+++ 680. Valid Palindrome II/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _680._Valid_Palindrome_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidPalindrome("aba"));
+            Console.WriteLine(ValidPalindrome("abca"));
+            Console.WriteLine(ValidPalindrome("abc"));
+            Console.WriteLine(ValidPalindrome("a"));
+            Console.WriteLine(ValidPalindrome("ab"));
+            Console.WriteLine(ValidPalindrome("abcdeffedcbaa"));
+            Console.WriteLine(ValidPalindrome("deeee"));
+            Console.WriteLine(ValidPalindrome("cbbcc"));
+        }
+
+        public static bool ValidPalindrome(string s)
+        {
+            int l = 0, r = s.Length - 1;
+            while (l < r)
+            {
+                if (s[l] != s[r])
+                    return (IsPalindrome(s, l+1, r) || IsPalindrome(s, l, r-1));
+                l++;
+                r--;
+            }
+            return true;
+        }
+
+        public static bool IsPalindrome(string s, int l, int r)
+        {
+            while (l < r)
+                if (s[l++] != s[r--]) return false;
+            return true;
+        }
+    }
+}
