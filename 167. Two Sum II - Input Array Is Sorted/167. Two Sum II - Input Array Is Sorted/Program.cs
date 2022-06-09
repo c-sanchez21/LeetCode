001a0c161b6277f14985265b4cc2fd9121066ca9
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-06-09 15:33:13 -0700

    Solution Added

---
+++ 167. Two Sum II - Input Array Is Sorted/167. Two Sum II - Input Array Is Sorted/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _167._Two_Sum_II___Input_Array_Is_Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",",TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
+            Console.WriteLine(String.Join(",", TwoSum(new int[] { 2, 3, 4 }, 6)));
+            Console.WriteLine(String.Join(",", TwoSum(new int[] { -1, 0 }, -1)));
+        }
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int[] vals = new int[20001];
+            int idx;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                idx = numbers[i];
+                if (idx != 0) idx += 1000;
+                vals[idx] = i + 1;
+            }
+
+            int val; 
+            for(int i =0; i < numbers.Length; i++)
+            {
+                val = numbers[i];
+                idx = target - val;
+                if (idx != 0) idx += 1000;
+                if (vals[idx] != 0)
+                    return new int[] { i + 1, vals[idx] };
+            }
+
+            return new int[] { 0, 0 };
+        }
+    }
+}
