2fe758666aedaa72fa17c4adba2dcb97db88db86
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-06-11 16:27:44 -0700

    Solution Added

---
+++ 1658. Minimum Operations to Reduce X to Zero/1658. Minimum Operations to Reduce X to Zero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1658._Minimum_Operations_to_Reduce_X_to_Zero
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinOperations(new int[] { 1, 1, 4, 2, 3 }, 5));
+        }
+
+        public static int MinOperations(int[] nums, int x)
+        {
+            int cur = 0;
+            foreach (int num in nums)
+                cur += num;
+            int n = nums.Length;
+            int min = int.MaxValue;
+            int l = 0;
+            for(int r = 0; r < n; r++)
+            {
+                cur -= nums[r];
+                while(cur < x && l <= r)
+                    cur += nums[l++];
+                if (cur == x)
+                    min = Math.Min(min, (n - 1 - r) + l);
+            }
+            return (min == int.MaxValue) ? -1 : min;
+        }
+    }
+}
