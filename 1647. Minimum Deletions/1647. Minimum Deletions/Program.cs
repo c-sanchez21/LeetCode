d1bf57241ae1497136ac3ef2c243124ecc0f6f4b
Cesar Sanchez <c_sanchez21@hotmail.com>
2022-06-30 14:50:55 -0700

    Solution Added

---
+++ 1647. Minimum Deletions/1647. Minimum Deletions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1647._Minimum_Deletions
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinDeletions("aab"));
+            Console.WriteLine(MinDeletions("aaabbbcc"));
+            Console.WriteLine(MinDeletions("ceabaacb"));
+        }
+
+        public static int MinDeletions(string s)
+        {
+            int[] arr = new int[26];
+            int idx;
+            for (int i = 0; i < s.Length; i++)
+            {
+                idx = s[i] - 'a';
+                arr[idx]++;
+            }
+            HashSet<int> set = new HashSet<int>();
+            int count;
+            int steps = 0;
+            for(int i =0;i < arr.Length;i++)
+            {
+                count = arr[i];
+                if (!set.Contains(count))
+                    set.Add(count);
+                else
+                {
+                    while (set.Contains(count) && count > 0)
+                    {
+                        count--;
+                        steps++;
+                    }
+                    if (count > 0)
+                        set.Add(count);
+                }
+            }
+            return steps;
+        }
+    }
+}
---
+++ 462. Minimum Moves to Equal Array Elements II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _462._Minimum_Moves_to_Equal_Array_Elements_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinMoves2(new int[] { 1, 2, 3 }));
+            Console.WriteLine(MinMoves2(new int[] { 1, 10, 2, 9 }));
+            Console.WriteLine(MinMoves2(new int[] { 1}));
+            Console.WriteLine(MinMoves2(new int[] { 1, 0, 0, 8, 6 }));//14
+        }
+
+        public static int MinMoves2(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            long min = int.MaxValue;
+            long sum;
+            foreach(int num in nums)
+            {
+                sum = 0;
+                foreach(int n in nums)
+                    sum += Math.Abs(num - n);
+                min = Math.Min(sum, min);
+            }
+            return (int)min;
+        }
+    }
+}
