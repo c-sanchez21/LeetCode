b6c1307ea463971512d4932ba3d14a3e32740aa5
Cesar Sanchez <c_sanchez21@hotmail.com>
2023-03-20 09:52:44 -0700

    Comments Added

--- 605. Can Place Flowers/Program.cs
+++ 605. Can Place Flowers/Program.cs
@@ -2,6 +2,7 @@
 
 namespace _605._Can_Place_Flowers
 {
+    //https://leetcode.com/problems/can-place-flowers/
     class Program
     {
         static void Main(string[] args)
@@ -14,7 +15,9 @@ namespace _605._Can_Place_Flowers
             int i = 0, count = 0;
             while (i < flowerbed.Length)
             {
-                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                if (flowerbed[i] == 0 && //Current == 0;
+                    (i == 0 || flowerbed[i - 1] == 0) && //Is Beginning or Previous == 0
+                    (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) //Is Last or Next == 0
                 {
                     flowerbed[i] = 1;
                     count++;
