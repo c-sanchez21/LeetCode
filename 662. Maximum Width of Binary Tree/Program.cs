5295e5ee68c1a50fece4d37f8d25c2ffb424db09
Cesar Sanchez <c_sanchez21@hotmail.com>
2023-04-21 14:16:07 -0700

    Solution Added

---
+++ 662. Maximum Width of Binary Tree/Program.cs
@@ -0,0 +1,50 @@
+﻿//https://leetcode.com/problems/maximum-width-of-binary-tree/
+Console.WriteLine("Hello, World!");
+
+
+int WidthOfBinaryTree(TreeNode root)
+{
+    return BFS(root);
+}
+
+int BFS(TreeNode n)
+{
+    Queue<KeyValuePair<TreeNode, int>> q = new Queue<KeyValuePair<TreeNode, int>>();
+    q.Enqueue(new KeyValuePair<TreeNode, int>(n, 0));
+
+    int maxWidth = 0;
+    int lvlLength, lvlStart, idx;
+    TreeNode node;
+    KeyValuePair<TreeNode, int> kvp;
+    while (q.Count > 0)
+    {
+        lvlLength = q.Count;
+        lvlStart = q.Peek().Value;
+        idx = 0;
+
+        for (int i = 0; i < lvlLength; i++)
+        {
+            kvp = q.Dequeue();
+            node = kvp.Key;
+            idx = kvp.Value;
+
+            if (node.left != null) q.Enqueue(new KeyValuePair<TreeNode, int>(node.left, 2 * idx));
+
+            if (node.right != null) q.Enqueue(new KeyValuePair<TreeNode, int>(node.right, 2 * idx + 1));
+        }
+        maxWidth = Math.Max(maxWidth, idx - lvlStart + 1);
+    }
+    return maxWidth;
+}
+
+//  Definition for a binary tree node.
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
---
+++ 879. Profitable Schemes/Program.cs
@@ -0,0 +1,30 @@
+﻿//https://leetcode.com/problems/profitable-schemes/
+
+Console.WriteLine("Hello, World!");
+const int mod = 1000000007;
+int[,,] cache = new int[101, 101, 101];
+
+int ProfitableSchemes(int n, int minProfit, int[] group, int[] profit)
+{
+    for (int i = 0; i <= group.Length; i++)
+        for (int j = 0; j <= n; j++)
+            for (int k = 0; k <= 100; k++)
+                cache[i, j, k] = -1;
+    return Find(0,0,0,n,minProfit,group, profit);
+}
+
+
+int Find(int pos, int count, int profit, int n, int minProfit, int[] group, int[] profits)
+{
+    if (pos == group.Length) return profit >= minProfit ? 1 : 0;
+
+    if (cache[pos, count, profit] != -1) return cache[pos, count, profit];
+
+    int totalWays = Find(pos+1,count, profit, n, minProfit, group, profits);
+    if (count + group[pos] <= n)
+            totalWays += Find(pos + 1, count + group[pos],
+            Math.Min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
+  
+    return cache[pos, count, profit] = totalWays % mod;
+}
+
